042ec0d59c030ccbf82e0d5a549385a3213017cb
mkpoli <git@mkpo.li>
2024-10-24 21:08:42 +0900

    feat: add basic content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Java is to JavaScript as X is to Y...</h1>
+
+<a href="http://javascriptisnotjava.com/">JavaScript is not Java.</a>
+
+<p>This is a list of things that are just like the different between Java and JavaScript in world's languages.</p>
+
+<h2>English</h2>
+<ul>
+  <li>Ham 🆚 Hamster</li>
+</ul>
+
+<h2>Chinese</h2>
+<ul>
+  <li>雷锋 🆚 雷峰塔</li>
+</ul>
+
+<h2>Japanese</h2>
+<ul>
+  <li>インド 🆚 インドネシア</li>
+</ul>
