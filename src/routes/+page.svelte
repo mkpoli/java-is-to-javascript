c7f5825ce8a579411ff5f6ff9fa4a20b928f0f4d
mkpoli <git@mkpo.li>
2024-10-25 02:10:47 +0900

    feat: add styling to all links

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -26,18 +26,21 @@
 <footer class="text-center my-4">
   <p>
     All data in this project is licensed under <a
+      class="text-blue-500 hover:underline"
       href="https://creativecommons.org/publicdomain/zero/1.0/legalcode"
       target="_blank">CC0 1.0 Universal License</a
     >
     (CC0-1.0).
   </p>
   <p>
-    The source code of the website is licensed under <a href="https://opensource.org/license/0bsd" target="_blank"
-      >Zero-Clause BSD License</a
+    The source code of the website is licensed under <a
+      class="text-blue-500 hover:underline"
+      href="https://opensource.org/license/0bsd"
+      target="_blank">Zero-Clause BSD License</a
     >
     (0BSD).
   </p>
   <p>
-    Copyright &copy; 2024 <a href="https://mkpo.li/" target="_blank">mkpoli</a>.
+    Copyright &copy; 2024 <a class="text-blue-500 hover:underline" href="https://mkpo.li/" target="_blank">mkpoli</a>.
   </p>
 </footer>
