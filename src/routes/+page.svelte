7cb0e6fb19b955df650f6ea471eb0eeca83ecf38
mkpoli <git@mkpo.li>
2024-10-24 21:42:51 +0900

    refactor: use js to manage data

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,46 @@
+<script lang="ts">
+  let DATA = [
+    {
+      language: 'English',
+      items: [
+        {
+          java: 'Ham',
+          javascript: 'Hamster',
+        },
+      ],
+    },
+    {
+      language: 'Chinese',
+      items: [
+        {
+          java: '雷锋',
+          javascript: '雷峰塔',
+        },
+      ],
+    },
+    {
+      language: 'Japanese',
+      items: [
+        {
+          java: 'インド',
+          javascript: 'インドネシア',
+        },
+      ],
+    },
+  ] as const;
+</script>
+
 <h1>Java is to JavaScript as X is to Y...</h1>
 
 <a href="http://javascriptisnotjava.com/">JavaScript is not Java.</a>
 
 <p>This is a list of things that are just like the different between Java and JavaScript in world's languages.</p>
 
-<h2>English</h2>
-<ul>
-  <li>Ham 🆚 Hamster</li>
-</ul>
-
-<h2>Chinese</h2>
-<ul>
-  <li>雷锋 🆚 雷峰塔</li>
-</ul>
-
-<h2>Japanese</h2>
-<ul>
-  <li>インド 🆚 インドネシア</li>
-</ul>
+{#each DATA as item}
+  <h2>{item.language}</h2>
+  <ul>
+    {#each item.items as i}
+      <li>{i.java} 🆚 {i.javascript}</li>
+    {/each}
+  </ul>
+{/each}
