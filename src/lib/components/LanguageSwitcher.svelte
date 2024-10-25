04eef012926f5ece3e5540f9062c0aa19de92f5b
mkpoli <git@mkpo.li>
2024-10-25 13:19:16 +0900

    feat: use both autonyms and xenonyms

--- src/lib/components/LanguageSwitcher.svelte
+++ src/lib/components/LanguageSwitcher.svelte
@@ -3,8 +3,12 @@
   import type { AvailableLanguageTag } from '$lib/paraglide/runtime';
   import { i18n } from '$lib/i18n';
   import { page } from '$app/stores';
-  const languageNames = new Intl.DisplayNames(languageTag() ?? 'en', { type: 'language' });
   import { goto } from '$app/navigation';
+
+  const languageAutonyms = new Map<AvailableLanguageTag, string>(
+    availableLanguageTags.map((lang) => [lang, new Intl.DisplayNames(lang, { type: 'language' }).of(lang) ?? lang])
+  );
+  const languageNameInCurrentLocale = new Intl.DisplayNames(languageTag() ?? 'en', { type: 'language' });
 
   function switchToLanguage(newLanguage: AvailableLanguageTag) {
     const canonicalPath = i18n.route($page.url.pathname);
@@ -15,8 +19,12 @@
 
 <div class="flex gap-2 justify-center">
   {#each availableLanguageTags as lang}
-    <button class="text-blue-500 hover:underline" onclick={() => switchToLanguage(lang)}>
-      {languageNames.of(lang)}
+    <button
+      title={`${languageNameInCurrentLocale.of(lang)} (${lang})`}
+      class="text-blue-500 hover:underline"
+      onclick={() => switchToLanguage(lang)}
+    >
+      {languageAutonyms.get(lang)}
     </button>
   {/each}
 </div>
